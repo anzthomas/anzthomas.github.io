1995664630bf22d50c2109d161122c756257611f
anzthomas <anzthomas491@gmail.com>
2025-10-02 02:47:22 +0530

    coma

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
    // {title:'Brahma HelpLine Telegram Bot', desc:'Python & Telegram API', img:'images/proj1.jpg', url:'https://github.com/anz-thomas/brahma-helpline'},
   //  {title:'GitAutoPusher', desc:'Bash script to auto push repos', img:'images/proj2.jpg', url:'https://github.com/anz-thomas/gitautopusher'},
   //  {title:'Object Tracker (Android)', desc:'Auto pan/zoom camera object tracking', img:'images/proj3.jpg', url:'#'}
-    {title:'Service hub', desc:'A ai based service hub', img:'images/proj3.jpg', url:'#'}
+    {title:'Service hub', desc:'A ai based service hub', img:'images/proj3.jpg', url:'#'},
     {title:'Smartixx', desc:'A online concession ticked booking system ', img:'images/proj3.jpg', url:'#'}
 
   ];
